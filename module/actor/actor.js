25b99ae547b497c51bfec58c9acc97162e85a355
Jason Reid <jason@jasonreid.com>
2020-06-03 05:14:22 -0400

    Drag and drop to slots

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -31,35 +31,6 @@ export class TorchbearerActor extends Actor {
   }
 
   removeItemFromInventory(itemId) {
-    const equip = this.getOwnedItem(itemId).data.data.equip;
-    const slotsVacated = this.getOwnedItem(itemId).data.data.slots;
-
-    // Add the slots being vacated back to slotsAvailable
-    switch (equip) {
-      case "Head":
-        this.update({'data.Head.wornSlotsAvailable': this.data.data.Head.wornSlotsAvailable + slotsVacated});
-        break;
-      case "Neck":
-        this.update({'data.Neck.wornSlotsAvailable': this.data.data.Neck.wornSlotsAvailable + slotsVacated});
-        break;
-      case "Hands (Worn)":
-        this.update({'data.Hands.wornSlotsAvailable': this.data.data.Hands.wornSlotsAvailable + slotsVacated});
-        break;
-      case "Hands (Carried)":
-        this.update({'data.Hands.carriedSlotsAvailable': this.data.data.Hands.carriedSlotsAvailable + slotsVacated});
-        break;
-      case "Feet":
-        this.update({'data.Feet.wornSlotsAvailable': this.data.data.Feet.wornSlotsAvailable + slotsVacated});
-        break;
-      case "Torso":
-        this.update({'data.Torso.wornSlotsAvailable': this.data.data.Torso.wornSlotsAvailable + slotsVacated});
-        break;
-      case "Belt":
-        this.update({'data.Belt.packSlotsAvailable': this.data.data.Belt.packSlotsAvailable + slotsVacated});
-        break;
-    }
-
-    // Delete the item
     this.deleteOwnedItem(itemId);
   }
 }
