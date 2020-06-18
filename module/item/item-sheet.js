30a8772f90073ffa09717cdd1345cf530a9a9444
Nick Serluco <nserluco@gmail.com>
2020-06-17 20:02:18 -0400

    Spells

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -19,7 +19,9 @@ export class TorchbearerItemSheet extends ItemSheet {
   get template() {
     const path = "systems/torchbearer/templates/item";
     // Return a single sheet for all item types.
-    return `${path}/item-sheet.html`;
+    // return `${path}/item-sheet.html`;
+    return `${path}/${this.item.data.type}-sheet.html`;
+
     // Alternatively, you could use the following return statement to do a
     // unique item sheet by type, like `weapon-sheet.html`.
 
