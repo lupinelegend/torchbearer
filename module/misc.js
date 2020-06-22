1b09f7aa8b0805fc2162d460f38ce86e46f95ba2
Jason Reid <jason@jasonreid.com>
2020-06-22 13:27:44 -0400

    Tidying up dialog

--- module/misc.js
+++ module/misc.js
@@ -1,18 +1,3 @@
 export const MasterSkillsList = () => {
-    //Extract the list from the first character we see
-    const list = [];
-    for(let i = 0; i < game.actors._source.length; i++) {
-        let char = game.actors._source[i];
-        if(char.type === 'Character') {
-            if(char.data.skills) {
-                for(let skill in char.data.skills) {
-                    if(char.data.skills.hasOwnProperty(skill)) {
-                        list.push(skill);
-                    }
-                }
-                return list;
-            }
-        }
-    }
-    return list;
+    return game.system.template.Actor.templates.common.skills;
 }
