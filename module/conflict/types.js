e784208ed2c0e64811538fbd089d75f51097af1a
Nick <nserluco@gmail.com>
2020-07-05 11:08:16 -0400

    New Conflict types

--- module/conflict/types.js
+++ module/conflict/types.js
@@ -1,46 +1,145 @@
 export const CONFLICT_TYPES = [
     {
-        name: 'Banish/Abjure',
-        skill: ['Arcanist', 'Ritualist', 'Nature'],
+        name: 'Negotiate',
+        disposition: ['Haggler'],
+        attack: ['Haggler'],
+        defend: ['Manipulator'],
+        feint: ['Manipulator'],
+        Maneuver: ['Haggler'],
         ability: 'Will'
     },
     {
+        name: 'Abjure',
+        disposition: ['Lore Master'],
+        attack: ['Lore Master'],
+        defend: ['Arcanist'],
+        feint: ['Lore Master'],
+        Maneuver: ['Arcanist'],
+        ability: 'Will'
+    },
+    {
+        name: 'Banish',
+        disposition: ['Theologian'],
+        attack: ['Theologian'],
+        defend: ['Ritualist'],
+        feint: ['Theologian'],
+        Maneuver: ['Ritualist'],
+        ability: 'Will'
+    },
+    {
+        name: 'Bind',
+        disposition: ['Theologian'],
+        attack: ['Theologian'],
+        defend: ['Theologian'],
+        feint: ['Lore Master'],
+        Maneuver: ['Lore Master'],
+        ability: 'Health'
+    },
+    {
+        name: 'Ambush',
+        disposition: ['Scout'],
+        attack: ['Fighter'],
+        defend: ['Rider', 'Scout'],
+        feint: ['Fighter'],
+        Maneuver: ['Rider', 'Scout'],
+        ability: 'Might'
+    },
+    {
+        name: 'Battle',
+        disposition: ['Strategist'],
+        attack: ['Commander'],
+        defend: ['Strategist'],
+        feint: ['Commander'],
+        Maneuver: ['Strategist'],
+        ability: 'Might'
+    },
+    {
+        name: 'Siege',
+        disposition: ['Strategist'],
+        attack: ['Sapper'],
+        defend: ['Strategist'],
+        feint: ['Sapper'],
+        Maneuver: ['Strategist'],
+        ability: 'Will'
+    },
+    {
+        name: 'Skirmish',
+        disposition: ['Commander'],
+        attack: ['Fighter'],
+        defend: ['Commander'],
+        feint: ['Fighter'],
+        Maneuver: ['Commander'],
+        ability: 'Might'
+    },
+    {
         name: 'Capture',
-        skill: ['Fighter', 'Hunter', 'Nature'],
+        disposition: ['Fighter', 'Hunter', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Will'
     },
     {
         name: 'Convince',
-        skill: ['Persuader', 'Nature'],
+        disposition: ['Persuader', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Will'
     },
     {
         name: 'Convince Crowd',
-        skill: ['Orator', 'Nature'],
+        disposition: ['Orator', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Will'
     },
     {
         name: 'Drive Off',
-        skill: ['Fighter', 'Nature'],
+        disposition: ['Fighter', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Health'
     },
     {
         name: 'Kill',
-        skill: ['Fighter', 'Nature'],
+        disposition: ['Fighter', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Health'
     },
     {
         name: 'Pursue/Flee',
-        skill: ['Scout', 'Rider', 'Nature'],
+        disposition: ['Scout', 'Rider', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Health'
     },
     {
         name: 'Trick/Riddle',
-        skill: ['Manipulator', 'Nature'],
+        disposition: ['Manipulator', 'Nature'],
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         ability: 'Will'
     },
     {
         name: 'Other',
+        attack: [],
+        defend: [],
+        feint: [],
+        maneuver: [],
         skill: [],
     }
 ];
